2105de979312101039486cba7bf2cd0dc61902b8
yehtut24 <43886898+yehtut24@users.noreply.github.com>
2018-10-16 11:54:53 +0100

    Pratical week 5

---
+++ pratical week 5.py
@@ -0,0 +1,26 @@
+a = 13
+b = 14
+print("a+1 !=b")    
+
+
+myage = input("Enter How old are you? ")
+print("Hi there,you are" + myage +"old")
+
+
+
+number1 = int(input("Enter first number"))
+number2 = int(input("Enter second number"))
+print("The result is "+str(number1+number2))
+
+average =(3+ 11+ 78+ 112+ 4+ 18) / 6
+print (average)
+
+number =int(input("Enter an integer : "))
+print("Remainder: " + str(number0% 7)
+
+scoreA = 1
+scoreB = 1
+if scoreA == scoreB :
+      print("Tie")
+
+      
